3e08dd40ee18e705e04744d42e593deb994b5461
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-08-21 16:31:34 +0530

    2nd commit

--- src/main/java/EXP2.java
+++ src/main/java/EXP2.java
@@ -1,8 +1,7 @@
 
 public class EXP2 {
-
 	public static void main(String[] args) {
-		System.out.println("Lab 2");
+		System.out.println("Lab 2...");
+		System.out.println("Trying Automation...");
 	}
-
 }
